554cc2bd9e9beeb993999cabba3807ddd808c051
G-shino <shinogu2929@gmail.com>
2020-05-28 03:26:06 +0900

    add test page

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { CreateElement } from "vue/types/umd";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
+  render: (h: CreateElement) => h(App)
 }).$mount("#app");
